8ca8ceee76bcbab09dcf0fa73ccbfcfcfd8ce5c3
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-06-15 22:55:56 +0600

    animation

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -32,15 +32,15 @@ const Banner = () => {
         //     <button className='button rounded-lg'>Hire Me!</button>
         //     </div>
         // </div>
-        <div className='w-full lg:w-3/4 mx-auto flex justify-center lg:justify-start items-center'>
-            <div>
+        <div className='w-full lg:w-3/4 mx-auto flex flex-col md:flex-row justify-center items-center mt-32 roaming-dots'>
+            <div className='w-full mx-auto px-4 lg:px-0 lg:mx-0 lg:w-4/6'>
             <p className='text-2xl md:text-7xl tracking-widest font-light '>Welcome!</p>
             <h1 className='text-xl md:text-6xl font-bold tracking-wider pt-4 pb-8'> <span className='text-white'> I am</span> {text}|
              </h1>
             <button onClick={handleDownload} className='btn btn-outline mr-8'>Resume</button>
             <button className='button rounded-lg'>Hire Me!</button>
             </div>
-            <div>
+            <div className='w-3/4 mx_auto lg:mx-0 lg:w-2/6'>
                 <img src={banner} alt="" />
             </div>
         </div>
